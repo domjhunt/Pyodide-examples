d9e06a3962ef1c6586f5d0ed2c545dff698f3407
domjhunt <86234171+domjhunt@users.noreply.github.com>
2023-03-28 22:37:17 +0100

    Added main test

--- binary_script.py
+++ binary_script.py
@@ -1,10 +1,18 @@
-num = int(input_num)
-final = []
-next_num = num
-for i in options:
-    if i > next_num:
-        final.append('0')
-    else:
-        final.append('1')
-        next_num = next_num % i
-''.join(final)
+options = [1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1]
+
+def binary_string(input_number):
+    num = int(input_number)
+    final = []
+    next_num = num
+    for i in options:
+        if i > next_num:
+            final.append('0')
+        else:
+            final.append('1')
+            next_num = next_num % i
+    return ''.join(final)
+
+
+if __name__ == "__main__":
+    binary_string(input_num)
+    
